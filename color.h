4c4d7ae33074143798e63ab550218aefa9d74a0c
Csilla Lukacs <erzsebet.lk@gmail.com>
2025-07-26 20:20:30 +0200

    more samples per pixel

--- color.h
+++ color.h
@@ -1,6 +1,7 @@
 #ifndef COLOR_H
 #define COLOR_H
 
+#include "interval.h"
 #include "vec3.h"
 
 using color = vec3;
@@ -10,9 +11,10 @@ void write_color(std::ostream& os, const color& pixel_color) {
     auto g = pixel_color.y();
     auto b = pixel_color.z();
 
-    int rbyte = int(r * 255.999);
-    int gbyte = int(g * 255.999);
-    int bbyte = int(b * 255.999);
+    static const interval intensity(0.000, 0.999);
+    int rbyte = int(256 * intensity.clamp(r));
+    int gbyte = int(256 * intensity.clamp(g));
+    int bbyte = int(256 * intensity.clamp(b));
 
     os << rbyte << " " << gbyte << " " << bbyte << "\n";
 }
