c26edc849fda321102f0371c28bfca32f105bb5d
Csilla Lukacs <erzsebet.lk@gmail.com>
2025-07-26 14:53:14 +0200

    hittable list and constants added

--- sphere.h
+++ sphere.h
@@ -2,11 +2,10 @@
 #define SPHERE_H
 
 #include "hittable.h"
-#include "vec3.h"
 
 class sphere : public hittable {
     public:
-        sphere(const point3& center, double radius) : center(center), radius(std::fmax(0, radius))) {}
+        sphere(const point3& center, double radius) : center(center), radius(std::fmax(0, radius)) {}
         bool hit(const ray& r, double tray_tmin, double ray_tmax, hit_record& rec) const override {
             vec3 oc = center - r.origin();
             auto a = r.direction().length_squared();
@@ -41,3 +40,5 @@ class sphere : public hittable {
         point3 center;
         double radius;
 };
+
+#endif
