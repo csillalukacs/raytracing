e4731126c547e05e3fded42c0e5274a4376ef197
Csilla Lukacs <erzsebet.lk@gmail.com>
2025-08-06 17:09:21 +0200

    checkered spheres scene

--- main.cpp
+++ main.cpp
@@ -85,6 +85,38 @@ void bouncing_spheres()
     std::clog << "Rendered in: " << std::chrono::duration_cast<std::chrono::seconds> (end - begin).count() << "s" << std::endl;
 }
 
+void checkered_spheres() {
+    hittable_list world;
+
+    auto checker = make_shared<checker_texture>(0.32, color(0.2, 0.3, 0.1), color(0.9, 0.9, 0.9));
+
+    world.add(make_shared<sphere>(point3(0,-10,0), 10, make_shared<lambertian>(checker)));
+    world.add(make_shared<sphere>(point3(0, 10, 0), 10, make_shared<lambertian>(checker)));
+
+    camera cam;
+
+    cam.aspect_ratio = 16.0 / 9.0;
+    cam.image_width = 400;
+    cam.samples_per_pixel = 100;
+    cam.max_depth = 50;
+
+    cam.vfov = 20;
+    cam.lookfrom = point3(13, 2, 3);
+    cam.lookat = point3(0, 0, 0);
+    cam.vup = vec3(0, 1, 0);
+
+    cam.defocus_angle = 0;
+
+    cam.render(world);
+}
+
 int main() {
-    bouncing_spheres();
+    switch (2) {
+        case 1:
+            bouncing_spheres();
+            break;
+        case 2:
+            checkered_spheres();
+            break;
+    }
 }
